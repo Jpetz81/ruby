c5f0a3fdfecb8bef8f0f5fbb255ea50748f75dc2
peaky76 <robertjamespeacock@gmail.com>
2023-02-14 09:41:01 +0000

    Add Tuesday tasks

---
+++ tuesday_tasks.rb
@@ -0,0 +1,74 @@
+# TASK ONE
+
+words = ["posterior", "tush", "rear", "ass", "arse", "backside", "bum", "bottom"]
+sorted_words = words # Use an array method here to sort the words into alphabetical order
+first_word = sorted_words # Select the first element from the sorted array
+
+# Take the above steps and turn them into a function by adding them into the function definition below
+def alphabetical_first(words)
+
+    # remember to return the final value
+end
+
+# Uncomment the line below to check it works
+more_words = ["todger", "member", "willy", "part", "johnson", "trousersnake", "phallus"]
+print(alphabetical_first(more_words))
+
+# TASK TWO
+
+price = 100
+vat_rate = 0.2
+# Calculate vat_payable here
+# Calculate price_plus_vat here
+
+# Take the above steps and use them in a function that will add vat to an item
+def add_vat(price)
+    vat_rate = 0.2
+    # remember to return the final value
+end
+
+# Uncomment the line below to check it works
+# print(add_vat(1567.53))
+
+# TASK THREE
+
+# Write a function that tells you true or false whether a number is divisible by 4
+def is_divisible_by_four(num)
+    # Hint: This can be done in one line, with an expression that uses the modulo (remainder) you learnt - % 
+end
+
+# Write a function that does the same but for four hundred
+def is_divisible_by_four_hundred(num)
+
+end
+
+# Now use the two above functions to calculate whether a year is a leap year
+def is_a_leap_year(year)
+    # The rule is:
+    # If a year is divisible by 4 it is a leap year, unless it is divisible by 400
+    # There are several ways to do this, but two things that might be helpful:
+    # '&&' means 'and', '!' means not
+
+end
+
+# Uncomment these to see if it works
+# print(is_a_leap_year(1976))
+# print(is_a_leap_year(1977))
+# print(is_a_leap_year(2000))
+
+# TASK FOUR
+days_in_months = [] # Write an array of integers representing the days in each month
+total_days = days_in_months # Use an array method here to total the days
+
+# Use the above, plus the leap year function to work out the number of days in a year
+def days_in_year(year)
+    # Calculate days in a normal year
+    # Add one if it is a leap year
+
+end
+
+# Uncomment these to see if it works
+# print(days_in_year(1976))
+# print(days_in_year(1977))
+# print(days_in_year(2000))
+
